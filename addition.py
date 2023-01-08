56f40e8c1d90f74286e3c31f0807d5718cfe329b
Subham Kumar <subhampco@gmail.com>
2023-01-08 11:19:05 +0000

    added python3 support in addition

--- addition.py
+++ addition.py
@@ -1,3 +1,7 @@
-print 'Adding Two Numbers 10 and 20 :',10+20
+#earlier this addition file is running on python2.7 now we are shifting on python3
+#commenting below line
+#print 'Adding Two Numbers 10 and 20 :',10+20
 print('How to color the git console?')
 print('git config --global color.ui auto')
+#creating with python3 in mind
+print('Adding two numbers 10 and 20: ',10+20)
